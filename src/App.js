26b845c060481a75c876e2b68e7aa8bd7ba260ce
Tahesha <taheshao@gmail.com>
2024-07-17 12:13:26 -0400

    Adding the style file import.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { getAllStarships } from './services/sw-api';
 import StarshipCard from './StarshipCard';
+import './style.css';
+
 
 function App() {
   const [starships, setStarships] = useState([]);

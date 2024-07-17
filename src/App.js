43e0c0adf5a9f88a2b168e22230f012654ffc4d4
Tahesha <taheshao@gmail.com>
2024-07-17 12:32:43 -0400

    Added video backgorund.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getAllStarships } from './services/sw-api';
-import StarshipCard from './StarshipCard';
+import StarshipCard from './StarshipCard'; // Correct import path
 import './style.css';
 import backgroundVideo from './assets/background.mp4';
 
@@ -13,15 +13,20 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Star Wars Starships</h1>
-      <div className="starship-list">
-        {starships.map((ship, index) => (
-          <StarshipCard key={index} name={ship.name} />
-        ))}
+      <video autoPlay loop muted className="video-background">
+        <source src={backgroundVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content">
+        <h1>Star Wars Starships</h1>
+        <div className="starship-list">
+          {starships.map((ship, index) => (
+            <StarshipCard key={index} name={ship.name} />
+          ))}
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
-
